47124c6c0ea3e21ad563b0934939200faf62ddef
Daniel Casas <56329807+JorgeCasas01@users.noreply.github.com>
2023-05-20 20:13:45 -0600

    Update Persona.java

--- SimulacionMercado/src/Persona.java
+++ SimulacionMercado/src/Persona.java
@@ -1,33 +1,34 @@
-  public class Persona extends Thread{
+
+public class Persona extends Thread{
   private String nombre;
   private boolean estaDescansando = false;
   private int tiempoDescanso;
-
+  
   public Persona(String parNombre, int parTiempoDesc){
     nombre = parNombre;
     tiempoDescanso = parTiempoDesc;
   }
-
+  
   public void setNombre(String parNombre){
     nombre = parNombre;
   }
-
+  
   public void setTiempoDescanso(int parTiempoDesc){
     tiempoDescanso = parTiempoDesc;
   }
-
+  
   public String getNombre(){
     return nombre;
   }
-
+  
   public int getTiempoDescanso(){
     return tiempoDescanso;
   }
-
+  
   public boolean getEstaDescansando(){
     return estaDescansando;
   }
-
+  
   public void descansar(){
    estaDescansando = true; 
   }
