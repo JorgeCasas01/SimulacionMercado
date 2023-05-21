1509d607478b96b281d2a669346e07a474082895
Daniel Casas <56329807+JorgeCasas01@users.noreply.github.com>
2023-05-20 20:11:50 -0600

    Update Consumidor.java

--- SimulacionMercado/src/Consumidor.java
+++ SimulacionMercado/src/Consumidor.java
@@ -1,5 +1,5 @@
-public class Consumidor extends Persona{
-  private int tiempoConsumo;
+public class Consumidor extends Persona {
+    private int tiempoConsumo;
   
   public Consumidor(String parNombre, int parTiempoDesc, int parTiempoConsumo) {
     super(parNombre, parTiempoDesc);
@@ -15,18 +15,8 @@ public class Consumidor extends Persona{
   }
   
   public void consumir(){
-    //ToDo
-  }
-  
-  public void run(){
-    while(!this.getEstaDescansando()){
-      this.consumir();
-      try {
-        Thread.sleep(5);
-      } catch (InterruptedException ex) {
-        Logger.getLogger(Consumidor.class.getName()).log(Level.SEVERE, null, ex);
-      }
+    if(!this.getEstaDescansando()){
+      //ToDo
     }
   }
-  
 }
