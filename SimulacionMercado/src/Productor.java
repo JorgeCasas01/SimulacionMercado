7adb251939073220cd158358f6a99ce98d94e336
infinite1000darc <47670144+infinite1000darc@users.noreply.github.com>
2023-05-20 19:13:08 -0600

    Update Productor.java

--- SimulacionMercado/src/Productor.java
+++ SimulacionMercado/src/Productor.java
@@ -1,11 +1,34 @@
 public class Productor extends Persona{
-    public int tiempoProduccion;
-
-    Productor(){
-        //this.tiempoProduccion=tiempoProduccion;
+  int tiempoProduccion;
+  
+  public Productor(String parNombre, int parTiempoDesc, int parTiempoProduccion) {
+    super(parNombre, parTiempoDesc);
+    tiempoProduccion = parTiempoProduccion;
+  }
+ 
+  public int returnTiempoProduccion(){
+    return tiempoProduccion;
+  }
+  
+  public void setTiempoProduccion(int parTiempoProduccion){
+    tiempoProduccion = parTiempoProduccion;
+  }
+  
+  public void producir(){
+    if(!this.getEstaDescansando()){
+      //ToDo
     }
-
-    public void Producir(){
-
+  }
+  
+  public void run(){
+    while(!this.getEstaDescansando()){
+      this.producir();
+      try {
+        Thread.sleep(5);
+      } catch (InterruptedException ex) {
+        Logger.getLogger(Productor.class.getName()).log(Level.SEVERE, null, ex);
+      }
     }
+  }
+  
 }
